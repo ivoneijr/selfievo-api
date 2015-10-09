53f0b969d276bbdfc6af795d6f875d2c8c25fbf8
Ivonei Junior <ivoneijr@gmail.com>
2015-10-09 00:31:28 -0300

    Change hardcode app uri to environment var

---
+++ config/initializers/dev_evironment.rb
@@ -0,0 +1,7 @@
+unless Rails.env.production?
+  ENV['APPLICATION_URL'] = "127.0.0.1"
+  ENV['APPLICATION_PORT'] = "3000"
+else
+	ENV['APPLICATION_URL'] = "mysterious-spire-8112.herokuapp.com"
+end
+
